eb6e6fb0c83e8e4c609da9e4155768a9f070783b
Helena Rodriguez Cagide <helenarodrica2@uoc.edu>
2024-05-18 00:06:33 +0200

    aplicadas transiciones, mejoras de diseño y optimización de archivos

--- src/app.js
+++ src/app.js
@@ -2,4 +2,20 @@ const darkModeButton = document.getElementById('temaOscuro');
 const body = document.body;
 darkModeButton.addEventListener('click', () => {
     body.classList.toggle('modo-oscuro');
-})
+});
+
+const whiteModeButton = document.getElementById('temaClaro');
+whiteModeButton.addEventListener('click', () => {
+    body.classList.toggle('modo-claro');
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPage = window.location.pathname;
+    const linksNav = document.querySelectorAll('.link-nav');
+
+    linksNav.forEach((link) => {
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+});
